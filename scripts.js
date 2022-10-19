ed1aa81991230b3011a7f68fccd35ec57cd08ea7
RedPho <medasdan@gmail.com>
2022-10-19 19:28:18 +0300

    Add some clear buttons and score table and finish the logic

--- scripts.js
+++ scripts.js
@@ -2,16 +2,15 @@ const playerFactory = (userName, XorO) => {
   score = 0; 
   return{ userName, score, XorO } };
 
-const firstUser = playerFactory("emin", "X");
-const secondUser = playerFactory("faruk", "O");
+const firstUser = playerFactory("Player 1", "X");
+const secondUser = playerFactory("Player 2", "O");
+let currentUser = playerFactory(firstUser.userName, firstUser.XorO);
+const result = document.querySelector("#result");
 
 let makeButtonsUnclickable = "no";
 
-let currentUser = playerFactory(firstUser.userName, firstUser.XorO);
-
-
 const gameboard = ( () => {
-  let table = ["X", "X", "O","X", "X", "X", "O", "O", "X"]
+  let table = []
   return { table };
 } )();
 
@@ -67,9 +66,10 @@ function setRaundWinner(){
   ((gt[2] == "X") && (gt[2] == gt[5]) && (gt[5] == gt[8])) ||
   ((gt[0] == "X") && (gt[0] == gt[4]) && (gt[4] == gt[8])) ||
   ((gt[2] == "X") && (gt[2] == gt[4]) && (gt[4] == gt[6])) ) {
-    console.log("" + firstUser.userName + " wins!");
+    result.innerText = "" + firstUser.userName + " wins!";
     makeButtonsUnclickable = "yes"
     firstUser.score++;
+    document.querySelector("#player1score").innerText = firstUser.score;
     console.log(firstUser);
   }
 
@@ -82,16 +82,71 @@ function setRaundWinner(){
     ((gt[2] == "O") && (gt[2] == gt[5]) && (gt[5] == gt[8])) ||
     ((gt[0] == "O") && (gt[0] == gt[4]) && (gt[4] == gt[8])) ||
     ((gt[2] == "O") && (gt[2] == gt[4]) && (gt[4] == gt[6])) ) {
-      console.log("" + secondUser.userName + " wins!");
+      result.innerText = "" + secondUser.userName + " wins!";
       makeButtonsUnclickable = "yes"
       secondUser.score++;
+      document.querySelector("#player2score").innerText = secondUser.score;
       console.log(secondUser);
     }
+  else {
+    let x = 0;
+    for(i = 0; i < 9; i++){
+      if(gt[i] != "") {
+        x++;
+        }
+      if(x == 9) {
+        result.innerText = "Tie!";
+      }
+    }
+  }
+    
+}
+
+function setPlayerNames(){
+  if ((document.querySelector("#player1").value == "") || (document.querySelector("#player2").value == "")){
+    alert("Please enter valid values for player names")
+  }
+  else{
+    firstUser.userName = document.querySelector("#player1").value;
+    secondUser.userName = document.querySelector("#player2").value;
+    console.log(firstUser.userName);
+    console.log(secondUser.userName);
+    currentUser = playerFactory(firstUser.userName, firstUser.XorO);
+    document.querySelector("#player1").value = "";
+    document.querySelector("#player2").value = "";
+    document.querySelector("#player1header").innerText = "" + firstUser.userName;
+    document.querySelector("#player2header").innerText = "" + secondUser.userName;
+  }
+  
+}
+
+const clearTable = function() {
+  for (i = 0; i < 9; i++) {
+    let blockToClear = document.getElementById(`${i}`);
+    blockToClear.innerText = "";
+  }
+  makeButtonsUnclickable = "no"
+  currentUser = playerFactory(firstUser.userName, firstUser.XorO);
+  updateTable();
+  result.innerText = "";
+}
+
+const clearScores = function(){
+  document.querySelector("#player1score").innerText = "0";
+  document.querySelector("#player2score").innerText = "0";
+  firstUser.score = 0;
+  secondUser.score = 0;
 }
 
 const game = ( () => {
+  let setPlayerNamesButton = document.querySelector("#setPlayers");
+  setPlayerNamesButton.addEventListener("click", setPlayerNames);
+  let clearButton = document.querySelector("#reset");
+  clearButton.addEventListener("click", clearTable)
+
+  let resetButton = document.querySelector("#resetScores");
+  resetButton.addEventListener("click", clearScores);
+
   updateTable();
-  
   console.log(gameboard.table);
 } )();
-
