f5d5fd4307c5d85de68488d0724e6c5e4ddecfd3
RedPho <medasdan@gmail.com>
2022-10-19 17:00:19 +0300

    Change users when one clicked a legit button

--- scripts.js
+++ scripts.js
@@ -1,22 +1,63 @@
-const playerFactory = (userName, score) => { 
-  return{ userName, score } };
+const playerFactory = (userName, XorO) => {
+  let score = 0; 
+  return{ userName, score, XorO } };
+
+const firstUser = playerFactory("emin", "X");
+const secondUser = playerFactory("faruk", "O");
+
+let currentUser = playerFactory(firstUser.userName, firstUser.XorO);
+
 
 const gameboard = ( () => {
-  let table = ["o", "x", "o","x", "x", "x", "o","x", "x"]
+  let table = ["X", "X", "O","X", "X", "X", "O", "O", "X"]
   return { table };
 } )();
 
 const table = gameboard.table;
 
-const updateTable = () =>  {
-  for(i = 0; i < table.length; i++){
-    let currentblock = document.getElementById(`${i}`);
-    currentblock.innerText = table[i];
+function updateTable() {
+  //Make buttons interactive
+  for(i = 0; i < 9; i++){
+    let currentBlock = document.getElementById(`${i}`);
+    currentBlock.addEventListener("click", () => {
+      if (currentBlock.innerText == ""){
+        if((currentUser.XorO) == "X"){
+          gameboard.table[i] = "X";
+          currentBlock.innerText = table[i];
+        }
+        else if(currentUser.XorO == "O"){
+          gameboard.table[i] = "O";
+          currentBlock.innerText = table[i];
+        }
+        for(i = 0; i < 9; i++) {
+          let currentBlock = document.getElementById(`${i}`);
+          gameboard.table[i] = currentBlock.innerText;
+        }
+
+        console.log(currentUser);
+        
+        if (currentUser.userName == firstUser.userName) {
+          currentUser.userName = secondUser.userName;
+          currentUser.XorO = secondUser.XorO;
+          console.log(currentUser);
+        }
+        else if (currentUser.userName == secondUser.userName) {
+          currentUser.userName = firstUser.userName;
+          currentUser.XorO = firstUser.XorO;
+          console.log(currentUser);
+        }
+
+        console.log(gameboard.table);
+      }
+      
+    })
   }
 }
 
+
 const game = ( () => {
+  updateTable()
+  
   console.log(table);
-  updateTable();
 } )();
 
