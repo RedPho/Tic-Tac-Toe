a66d9cc3c8453d5d8f11ae9509a574d23ab3e8fd
RedPho <medasdan@gmail.com>
2022-10-19 15:58:38 +0300

    Make table and render with js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,22 @@
-const playerFactory = (userName, score) => { return{ userName, score } };
-const ich = playerFactory("emin", 8);
+const playerFactory = (userName, score) => { 
+  return{ userName, score } };
 
 const gameboard = ( () => {
-  let table = ["x", "o","x", "x"]
+  let table = ["o", "x", "o","x", "x", "x", "o","x", "x"]
   return { table };
 } )();
 
+const table = gameboard.table;
 
+const updateTable = () =>  {
+  for(i = 0; i < table.length; i++){
+    let currentblock = document.getElementById(`${i}`);
+    currentblock.innerText = table[i];
+  }
+}
 
 const game = ( () => {
-  console.log(gameboard.table);
-  console.log("hey");
-  
+  console.log(table);
+  updateTable();
 } )();
+
